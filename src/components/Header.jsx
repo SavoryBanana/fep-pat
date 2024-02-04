b5310451159396440e7b0316a93695273203281a
SavoryBanana <patrickmayor.donghil@bicol-u.edu.ph>
2024-02-04 21:04:57 +0800

    Update header to support dark mode and light mode theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return(
         <>
-            <header className="flex flex-row sticky top-0 w-full h-16 place-items-center justify-around z-50 drop-shadow-sm">
+            <header className="flex flex-row sticky top-0 w-full h-16 place-items-center justify-around z-50 drop-shadow bg-white bg-opacity-50 backdrop-blur-lg dark:bg-header dark:bg-opacity-50">
                 <h1 className="text-xl m-4 font-bold"><a href="https://github.com/SavoryBanana" target="_blank" rel="noreferrer">@SavoryBanana</a></h1>
                 <ul className="grid grid-cols-4 gap-4 justify-end text-center">
                     <li className="p-2"><a href="#">Home</a></li>
