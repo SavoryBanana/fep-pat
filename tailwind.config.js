f6b221abf36a6a673e80570bde38e0374f0040fc
SavoryBanana <patrickmayor.donghil@bicol-u.edu.ph>
2024-02-04 15:30:12 +0800

    Fix PostCSS and Tailwind setup mistakes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["*.html","./src/*.jsx", "./src/*/.jsx"],
+  content: ["./src/*.jsx", "./src/*/*.jsx"],
   theme: {
     extend: {},
   },
